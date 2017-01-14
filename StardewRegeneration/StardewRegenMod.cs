0fd60521c21e530417d411d5f3897e4f9d0fc71e
Tonttu <tonttu86@msn.com>
2017-01-14 15:44:04 +0200

    Initial project configuration.

---
+++ StardewRegeneration/StardewRegenMod.cs
@@ -0,0 +1,15 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonttu.StardewValley.Mods.StardewRegeneration {
+    public class StardewRegenMod : Mod {
+
+        public override void Entry(IModHelper helper) {
+            Monitor.Log("Stardew Regeneration initialized.");
+        }
+    }
+}
