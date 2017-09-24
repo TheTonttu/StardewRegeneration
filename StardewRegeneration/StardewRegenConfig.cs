1af6ba765aec84c4d6307c88a925c14ebb865cb0
Tonttu <tonttu86@msn.com>
2017-09-24 12:48:32 +0300

    License under GNU GPLv3+ #2
    For some reason first commit did not include all lines from config class
    file...

--- StardewRegeneration/StardewRegenConfig.cs
+++ StardewRegeneration/StardewRegenConfig.cs
@@ -1,10 +1,13 @@
 ﻿#region copyright
 // StardewRegeneration - StardewRegenConfig.cs
+// Copyright © 2017 Tonttu and StardewRegeneration contributors
 // 
+// StardewRegeneration is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
+// StardewRegeneration is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
